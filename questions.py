050fb3c33ac9cb61e5368a3ea61f0738fa94cbf6
Drew W Mercer <japanexpress@gmail.com>
2022-08-14 10:39:33 -0400

    Initial commit of questions file

---
+++ questions.py
@@ -0,0 +1,31 @@
+import json
+import random
+
+f = open('questions.json')
+
+questions_dict = json.load(f)
+all_the_questions = list(questioms_dict["questions_list"])
+
+
+""" Fetch a random question array from the data  
+    and return it """
+
+
+def getRandomQuestion(questions_list):
+    random_num = random.randint(0, 5)
+    question_obj = questions_list[random_num]
+    return question_obj
+
+
+""" Plug the information from the random question array into
+    the html and return it """
+
+
+def printQuestion():
+    # get random question
+    random_question = getRandomQuestion(all_the_questions)
+
+    return random_question
+
+
+f.close()
