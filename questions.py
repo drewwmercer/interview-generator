541db219377884f8735c8609f388b1d74ba5ea73
Drew W Mercer <japanexpress@gmail.com>
2023-10-13 08:32:39 -0400

    Adding todo for better logic

--- questions.py
+++ questions.py
@@ -10,9 +10,9 @@ all_the_questions = list(questions_dict["questions_list"])
 """ Fetch a random question array from the data  
     and return it """
 
-
+# TODO - need to make this better
 def getRandomQuestion(questions_list):
-    random_num = random.randint(0, 13)
+    random_num = random.randint(0, 13) 
     question_obj = questions_list[random_num]
     return question_obj
 
